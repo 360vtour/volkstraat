d4fb94fa812963883f37800e96e54675c121cb42
360vtour <m4st4@outlook.com>
2023-11-24 18:07:04 +0100

    first commit

---
+++ data.js
@@ -0,0 +1,369 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-woonkamer",
+      "name": "Woonkamer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.830922810611062,
+        "pitch": 0.1464296787320265,
+        "fov": 1.2017224314627377
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.962087611673878,
+          "pitch": 0.007520804834815209,
+          "rotation": 6.283185307179586,
+          "target": "1-keuken"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-keuken",
+      "name": "Keuken",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.8074304887337185,
+        "pitch": 0.24626507764326533,
+        "fov": 1.2017224314627377
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3189780066750814,
+          "pitch": 0.14377107272613898,
+          "rotation": 0,
+          "target": "0-woonkamer"
+        },
+        {
+          "yaw": -2.539884457470098,
+          "pitch": 0.018721077389960428,
+          "rotation": 0,
+          "target": "6-slaapkamer-2"
+        },
+        {
+          "yaw": 2.973948649492197,
+          "pitch": 0.012018028054679775,
+          "rotation": 0,
+          "target": "2-inkom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-inkom",
+      "name": "Inkom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.5834383816370696,
+        "pitch": 0.08014883732238864,
+        "fov": 1.2017224314627377
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.04068939676726302,
+          "pitch": 0.055283863166048164,
+          "rotation": 0,
+          "target": "4-toilet"
+        },
+        {
+          "yaw": 0.8218984027492375,
+          "pitch": 0.06752742615731044,
+          "rotation": 0,
+          "target": "5-slaapkamer-1"
+        },
+        {
+          "yaw": -1.9412001901245723,
+          "pitch": 0.03270234463794175,
+          "rotation": 0,
+          "target": "3-badkamer"
+        },
+        {
+          "yaw": -2.877453785466166,
+          "pitch": 0.10760695525735464,
+          "rotation": 0,
+          "target": "1-keuken"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-badkamer",
+      "name": "Badkamer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.09973733487622916,
+        "pitch": 0.37195651190408974,
+        "fov": 1.2017224314627377
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3781480111435656,
+          "pitch": 0.007731503247345728,
+          "rotation": 0,
+          "target": "2-inkom"
+        },
+        {
+          "yaw": 2.110685521013429,
+          "pitch": 0.008273263412792886,
+          "rotation": 0,
+          "target": "5-slaapkamer-1"
+        },
+        {
+          "yaw": -2.9260535225518964,
+          "pitch": 0.01839869065390687,
+          "rotation": 0,
+          "target": "1-keuken"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-toilet",
+      "name": "Toilet",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.05641492472377152,
+        "pitch": 0.5961075723811042,
+        "fov": 1.2017224314627377
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1374821743350267,
+          "pitch": 0.1266240648382464,
+          "rotation": 0,
+          "target": "2-inkom"
+        },
+        {
+          "yaw": 2.4699959925576316,
+          "pitch": 0.10797890002322674,
+          "rotation": 0,
+          "target": "5-slaapkamer-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-slaapkamer-1",
+      "name": "Slaapkamer 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.45892083869945566,
+        "pitch": 0.2516548861320125,
+        "fov": 1.2017224314627377
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0081346106766524,
+          "pitch": -0.03099564159876067,
+          "rotation": 0,
+          "target": "2-inkom"
+        },
+        {
+          "yaw": -3.133751088730561,
+          "pitch": -0.02995987990714255,
+          "rotation": 0,
+          "target": "4-toilet"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-slaapkamer-2",
+      "name": "Slaapkamer 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.4387702542402856,
+        "pitch": 0.3449642601787293,
+        "fov": 1.2017224314627377
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1390286962568137,
+          "pitch": 0.057397516415612415,
+          "rotation": 0,
+          "target": "1-keuken"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-terras",
+      "name": "Terras",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.20671079648919388,
+        "pitch": 0.464663962047414,
+        "fov": 1.2017224314627377
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.544695942211842,
+          "pitch": 0.0243300011198766,
+          "rotation": 0,
+          "target": "6-slaapkamer-2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Volkstraat",
+  "settings": {
+    "mouseViewMode": "qtvr",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
